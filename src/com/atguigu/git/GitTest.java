5202b87b2066d65e8b4aaf67cd4ebf4b66f51c5d
dpf-up <64675347+dpf-up@users.noreply.github.com>
2021-11-28 22:58:24 +0800

    pull commit

--- src/com/atguigu/git/GitTest.java
+++ src/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4 ");
         System.out.println("hello hot fix ");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
